0efb101badc95f9bc58b9fb44e2441b8a2973dfb
Jelle42 <jelle.roessink@hotmail.com>
2026-06-01 17:36:31 +0200

    did not test yet

--- sinkhorn.py
+++ sinkhorn.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-def sinkhorn(K: np.ndarray, C: np.ndarray, a: np.ndarray, b: np.ndarray, precision: float = 1e-3):
+def sinkhorn(K: np.ndarray, a: np.ndarray, b: np.ndarray, precision: float = 1e-3):
     '''
-    Computes OT matrix P by finding u and v st P_ij^y = u_i e^(-C_ij/y) v_j
+    Computes OT matrix P by finding u and v st P = diag(u) K diag(v)
     '''
     n, m = np.shape(K)
+
+    assert np.shape(a)[0] == m, "a or K is not of desired shape"
+    assert np.shape(b)[0] == n, "b or K is not of desired shap"
 
     v = np.ones(n)
 
@@ -16,3 +19,9 @@ def sinkhorn(K: np.ndarray, C: np.ndarray, a: np.ndarray, b: np.ndarray, precisi
 
         if np.linalg.norm(v_prev - v) + np.linalg.norm(u_prev - u) < precision:
             break
+
+    return np.diag(u) @ K @ np.diag(v) 
+
+
+if __name__ == "__main__":
+    pass
